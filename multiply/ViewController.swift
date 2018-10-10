d0044a77d6b812f04242c4676377b9da39c39fb9
LEO CANTU <rl300126@rlas-macair-39.rlas-116.org>
2018-10-10 10:47:17 -0500

    Initial Commit

---
+++ multiply/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  multiply
+//
+//  Created by LEO CANTU on 10/10/18.
+//  Copyright © 2018 LEO CANTU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
